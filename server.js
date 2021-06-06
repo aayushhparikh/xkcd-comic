f643b77446b0d36be62cb4bf2e55ba8373158953
aayushhparikh <thedude5190@gmail.com>
2021-06-06 00:38:57 -0400

    added cors

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = normalizePort(porcess.env.PORT ||  5000)
 const app = express()
 const dev = app.get('env') !== 'production'
 
+app.use(cors())
+
 if(!dev) {
     app.use(compression())
     app.use(morgan('common'))
