a6b798e9aabd432ba4db1f7657866fb759a0e0f6
aayushhparikh <thedude5190@gmail.com>
2021-06-07 03:32:53 -0400

    added ability to detect newest comic

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import axios from 'axios'
 function App() {
 
   const [com, setCom] = useState()
+  const [newCom, setNewCom] = useState()
 
   const xkcdAPI = async() => {
     try {
       //https://corsporxy-xkcd.herokuapp.com/ allows for cross-origin requests to xkcd
       axios.get("https://corsporxy-xkcd.herokuapp.com/http://xkcd.com/info.0.json")
-      .then(res => setCom(res.data))
-    } catch (error) {
+      .then(res => 
+        setCom(res.data))
+    }catch (error) {
+      console.log(error)
+    }
+  }
+
+  const newestCom = async() => {
+    try {
+      //https://corsporxy-xkcd.herokuapp.com/ allows for cross-origin requests to xkcd
+      axios.get("https://corsporxy-xkcd.herokuapp.com/http://xkcd.com/info.0.json")
+      .then(res => setNewCom(res.data.num))
+    }catch (error) {
       console.log(error)
     }
   }
 
   useEffect(() => {
     xkcdAPI()
+  }, [])
+
+  useEffect(() => {
+    newestCom()
   }, [])
 
   const readCom = (number) => {
@@ -38,10 +54,15 @@ function App() {
   return (
     <div className="App">
       {com.title}
+      <p>
+        {com.year}, {com.month}, {com.day}
+      </p>
       <button onClick={() => readCom(com.num - 1)}>
         previous
       </button>
-      <button onClick={() => readCom(com.num + 1)}>
+      <button 
+      disabled={com.num === newCom}
+      onClick={() => readCom(com.num + 1)}>
         next
       </button>
       <img src={com.img} />
